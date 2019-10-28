be7cf9a5fdaf0d2e2f8b08f5fc3913cf6cfd1cd9
s3rfin <serfin009@gmail.com>
2019-10-28 23:31:05 +0100

    Added filter for EFCore logging

--- src/BeComfy.Services.Flights/Program.cs
+++ src/BeComfy.Services.Flights/Program.cs
@@ -19,6 +19,10 @@ namespace BeComfy.Services.Flights
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging((context, logging) => {
+                    logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Warning);
+                    logging.AddFilter("Microsoft.EntityFrameworkCore.Infrastructure", LogLevel.Warning);
+                });
     }
 }
